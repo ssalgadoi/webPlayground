38b54bb234f03eb2ff48c5d2002b720b54d5e55c
salgadoIbacache <se.salgado@duocuc.cl>
2023-09-19 23:22:33 -0300

    Migramos la app

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', wiews.home, name="home"),
+    path('', views.home, name="home"),
     path('sample/', views.sample, name="sample")
 ]
