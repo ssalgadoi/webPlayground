adfe5ce7e9ca6138eea4ddbf2a7263ee183b3aa2
salgadoIbacache <se.salgado@duocuc.cl>
2023-09-20 10:47:38 -0300

    Sea grega funcion Get a HomePageViews

--- core/views.py
+++ core/views.py
@@ -6,7 +6,6 @@ from django.shortcuts import render
 class HomePageView(TemplateView):
     template_name = "core/home.html"
     
-    
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name, {'title': "WebPlayground"})
 
