ff3a1915726072f16e215192b8d2759de9cd7833
salgadoIbacache <se.salgado@duocuc.cl>
2023-09-20 10:45:10 -0300

    Agregamos a home la funcion Get

--- core/views.py
+++ core/views.py
@@ -1,9 +1,14 @@
+from django.views.generic.base import TemplateView
 from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
-    return render(request, "core/home.html")
+class HomePageView(TemplateView):
+    template_name = "core/home.html"
+    
+    
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {'title': "WebPlayground"})
 
-def sample(request):
-    return render(request, "core/sample.html")
+class SamplePageView(TemplateView):
+    template_name = "core/sample.html"
