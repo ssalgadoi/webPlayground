fae021473f8c801cf9899ab4182360a891847437
salgadoIbacache <se.salgado@duocuc.cl>
2023-09-21 18:56:37 -0300

    Agregamos PageCreate

--- pages/urls.py
+++ pages/urls.py
@@ -4,5 +4,5 @@ from .views import PageListView, PageDetailView, PageCreate
 pages_patterns = ([
     path('', PageListView.as_view(), name='pages'),
     path('<int:pk>/<slug:slug>/', PageDetailView.as_view(), name='page'),
-    path('create/', PageCreate.as_view, name='create' )
-]), 'pages'
+    path('create/', PageCreate.as_view(), name='create' )
+], 'pages')
