7969a39ad2848a714ec9883088ffda14cd6c40b9
salgadoIbacache <se.salgado@duocuc.cl>
2023-09-22 17:32:16 -0300

    Se elimina about

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import HomePageView, SamplePageView, AboutPageView
+from .views import HomePageView, SamplePageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name="home"),
-     path('about/', AboutPageView.as_view(), name="about"),
     path('sample/', SamplePageView.as_view(), name="sample")
 ]
