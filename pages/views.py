961987a58a6ac7b9170057b6c74546fbd0c26fa6
salgadoIbacache <se.salgado@duocuc.cl>
2023-09-22 17:34:41 -0300

    Se mo difica nombra a PageUpdate

--- pages/views.py
+++ pages/views.py
@@ -20,7 +20,7 @@ class PageCreate(CreateView):
     fields = ['title', 'content', 'order']
     success_url = reverse_lazy('pages:pages')
 
-class PageUpdateView(UpdateView):
+class PageUpdate(UpdateView):
     model = Page
     fields = ['title', 'content', 'order']
     template_name_suffix = "_update_form"
