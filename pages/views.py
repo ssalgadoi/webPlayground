3d29698da569a5a2d222793ae0b1166a2f7aea25
salgadoIbacache <se.salgado@duocuc.cl>
2023-09-23 19:03:17 -0300

    Agregamos la class PageDelete

--- pages/views.py
+++ pages/views.py
@@ -5,6 +5,8 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse, reverse_lazy
 from .models import Page
 
+
+
 # Create your views here.
 class PageListView(ListView):
     # pages = get_list_or_404(Page)
@@ -27,3 +29,8 @@ class PageUpdate(UpdateView):
     
     def get_success_url(self):
         return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
+    
+
+class PageDelete(DeleteView):
+    model = Page
+    success_url = reverse_lazy('pages:pages')
