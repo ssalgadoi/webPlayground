69cc6f7698b163cf0c98cd64bd2c7476911f1ca4
salgadoIbacache <se.salgado@duocuc.cl>
2023-09-25 09:04:19 -0300

    Agregamos registration a settings

--- webPlaygound/settings.py
+++ webPlaygound/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'registration',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -140,3 +141,7 @@ CKEDITOR_CONFIGS = {
         'width': 800,
     },
 }
+
+# Auth redirects
+# LOGIN_REDIRECT_URL = 'pages:pages'
+LOGOUT_REDIRECT_URL = 'home'
