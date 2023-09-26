45061deaa8a159b19789dc49b00191c580c79f9a
salgadoIbacache <se.salgado@duocuc.cl>
2023-09-26 13:54:08 -0300

    Se suben cambios

--- webPlaygound/urls.py
+++ webPlaygound/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from pages.urls import pages_patterns
+from profiles.urls import profiles_patterns
 from django.conf import settings
 
 urlpatterns = [
@@ -26,6 +27,8 @@ urlpatterns = [
     # Paths de Auth
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('registration.urls')),
+    # Paths de profiles
+    path('profiles/', include(profiles_patterns)),
 ]
 
 if settings.DEBUG:
