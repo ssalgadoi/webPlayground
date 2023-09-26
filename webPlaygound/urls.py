257e00e97b92dbd341e15223dada9b0555e1d20a
salgadoIbacache <se.salgado@duocuc.cl>
2023-09-26 15:59:36 -0300

    Se agreg appMessenger al url general

--- webPlaygound/urls.py
+++ webPlaygound/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from pages.urls import pages_patterns
 from profiles.urls import profiles_patterns
+from messenger.urls import messenger_patterns
 from django.conf import settings
 
 urlpatterns = [
@@ -29,6 +30,8 @@ urlpatterns = [
     path('accounts/', include('registration.urls')),
     # Paths de profiles
     path('profiles/', include(profiles_patterns)),
+     # Paths de Messenger
+    path('messenger/', include(messenger_patterns)),
 ]
 
 if settings.DEBUG:
