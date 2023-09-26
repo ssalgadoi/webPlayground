7d1433d1a5f287df82abb2dcb77fb3eb70169e6a
salgadoIbacache <se.salgado@duocuc.cl>
2023-09-26 06:52:35 -0300

    Se agrega EmailUpdate

--- registration/views.py
+++ registration/views.py
@@ -1,10 +1,11 @@
-from .forms import UserCreationFormWithEmail
+from .forms import UserCreationFormWithEmail, ProfileForm, EmailForm
 from django.views.generic import CreateView
 from django.views.generic.edit import UpdateView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from django import forms
+from .models import Profile
 
 # Create your views here.
 
@@ -36,6 +37,30 @@ class SignUpView(CreateView):
         return email
 
 
+
 @method_decorator(login_required, name='dispatch')
 class ProfileUpdate(UpdateView):
+    form_class = ProfileForm
+    success_url = reverse_lazy('profile')
     template_name = 'registration/profile_form.html'
+
+    def get_object(self):
+        # recuperar el objeto que se va editar
+        profile, created = Profile.objects.get_or_create(user=self.request.user)
+        return profile
+
+@method_decorator(login_required, name='dispatch')
+class EmailUpdate(UpdateView):
+    form_class = EmailForm
+    success_url = reverse_lazy('profile')
+    template_name = 'registration/profile_email_form.html'
+    
+    def get_object(self):
+        return self.request.user
+    
+    
+    def get_form(self, form_class=None):
+        form = super(EmailUpdate, self).get_form()
+        form.fields['email'].widget = forms.EmailInput(
+            attrs={'class':'form-control mb-2', 'placeholder': 'Email'})
+        return form
